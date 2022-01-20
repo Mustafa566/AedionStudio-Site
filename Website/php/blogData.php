c6534ab0139f144ef60728657fe65e0e14b2e928
Mustafa566 <50444816+Mustafa566@users.noreply.github.com>
2022-01-20 14:51:42 +0100

    carousel works with images

--- Website/php/blogData.php
+++ Website/php/blogData.php
@@ -19,157 +19,159 @@
                 '. $result['blogDescription'] .'
             </p>
         </div>
-    </div>
+    </div>';
 
-<div class="gap"></div>
-    <div class="carousel">
-        <ul class="slides">
-    ';
-    if ($result["image"] != null) {
-        echo '
-        <input type="radio" name="radio-buttons" id="img-1" checked/>
-        <li class="slide-container">
-            <div class="slide-image">
-                <img src="data:image/;base64,'.base64_encode($result["image"]).'"/>
-            </div>
-            <div class="carousel-controls">
-                <label for="img-6" class="prev-slide">
-                    <span>&lsaquo;</span>
-                </label>
-                <label for="img-2" class="next-slide">
-                    <span>&rsaquo;</span>
-                </label>
-            </div>
-        </li>
+if ($result["image"] != null) {
+    echo '<div class="gap"></div>
+        <div class="carousel">
+            <ul class="slides">
         ';
-    }
-    if ($result["image2"] != null) {
-        echo '
-        <input type="radio" name="radio-buttons" id="img-2"/>
-        <li class="slide-container">
-            <div class="slide-image">
-                <img src="data:image/;base64,'.base64_encode($result["image2"]).'"/>
-            </div>
-            <div class="carousel-controls">
-                <label for="img-1" class="prev-slide">
-                    <span>&lsaquo;</span>
-                </label>
-                <label for="img-3" class="next-slide">
-                    <span>&rsaquo;</span>
-                </label>
-            </div>
-        </li>
-        ';
-    }
-    if ($result["image3"] != null) {
-        echo '
-        <input type="radio" name="radio-buttons" id="img-3"/>
-        <li class="slide-container">
-            <div class="slide-image">
-                <img src="data:image/;base64,'.base64_encode($result["image3"]).'"/>
-            </div>
-            <div class="carousel-controls">
-                <label for="img-2" class="prev-slide">
-                    <span>&lsaquo;</span>
-                </label>
-                <label for="img-4" class="next-slide">
-                    <span>&rsaquo;</span>
-                </label>
-            </div>
-        </li>
-        ';
-    }
-    if ($result["image4"] != null) {
-        echo '
-        <input type="radio" name="radio-buttons" id="img-4"/>
-        <li class="slide-container">
-            <div class="slide-image">
-                <img src="data:image/;base64,'.base64_encode($result["image4"]).'"/>
-            </div>
-            <div class="carousel-controls">
-                <label for="img-3" class="prev-slide">
-                    <span>&lsaquo;</span>
-                </label>
-                <label for="img-5" class="next-slide">
-                    <span>&rsaquo;</span>
-                </label>
-            </div>
-        </li>
-        ';
-    }
-    if ($result["image5"] != null) {
-        echo '
-        <input type="radio" name="radio-buttons" id="img-5"/>
-        <li class="slide-container">
-            <div class="slide-image">
-                <img src="data:image/;base64,'.base64_encode($result["image5"]).'"/>
-            </div>
-            <div class="carousel-controls">
-                <label for="img-4" class="prev-slide">
-                    <span>&lsaquo;</span>
-                </label>
-                <label for="img-6" class="next-slide">
-                    <span>&rsaquo;</span>
-                </label>
-            </div>
-        </li>
-        ';
-    }
-    if ($result["image5"] != null) {
-        echo '
-        <input type="radio" name="radio-buttons" id="img-6"/>
-        <li class="slide-container">
-            <div class="slide-image">
-                <img src="data:image/;base64,'.base64_encode($result["image6"]).'"/>
-            </div>
-            <div class="carousel-controls">
-                <label for="img-5" class="prev-slide">
-                    <span>&lsaquo;</span>
-                </label>
-                <label for="img-1" class="next-slide">
-                    <span>&rsaquo;</span>
-                </label>
-            </div>
-        </li>
-        ';
-    }
-    echo '<div class="carousel-dots">';
         if ($result["image"] != null) {
-            echo '<label for="img-1" class="carousel-dot" id="img-dot-1"></label>';
+            echo '
+            <input type="radio" name="radio-buttons" id="img-1" checked/>
+            <li class="slide-container">
+                <div class="slide-image">
+                    <img src="data:image/;base64,'.base64_encode($result["image"]).'"/>
+                </div>
+                <div class="carousel-controls">
+                    <label for="img-6" class="prev-slide">
+                        <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-2" class="next-slide">
+                        <span>&rsaquo;</span>
+                    </label>
+                </div>
+            </li>
+            ';
         }
         if ($result["image2"] != null) {
-            echo '<label for="img-2" class="carousel-dot" id="img-dot-2"></label>';
+            echo '
+            <input type="radio" name="radio-buttons" id="img-2"/>
+            <li class="slide-container">
+                <div class="slide-image">
+                    <img src="data:image/;base64,'.base64_encode($result["image2"]).'"/>
+                </div>
+                <div class="carousel-controls">
+                    <label for="img-1" class="prev-slide">
+                        <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-3" class="next-slide">
+                        <span>&rsaquo;</span>
+                    </label>
+                </div>
+            </li>
+            ';
         }
         if ($result["image3"] != null) {
-            echo '<label for="img-3" class="carousel-dot" id="img-dot-3"></label>';
+            echo '
+            <input type="radio" name="radio-buttons" id="img-3"/>
+            <li class="slide-container">
+                <div class="slide-image">
+                    <img src="data:image/;base64,'.base64_encode($result["image3"]).'"/>
+                </div>
+                <div class="carousel-controls">
+                    <label for="img-2" class="prev-slide">
+                        <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-4" class="next-slide">
+                        <span>&rsaquo;</span>
+                    </label>
+                </div>
+            </li>
+            ';
         }
         if ($result["image4"] != null) {
-            echo '<label for="img-4" class="carousel-dot" id="img-dot-4"></label>';
+            echo '
+            <input type="radio" name="radio-buttons" id="img-4"/>
+            <li class="slide-container">
+                <div class="slide-image">
+                    <img src="data:image/;base64,'.base64_encode($result["image4"]).'"/>
+                </div>
+                <div class="carousel-controls">
+                    <label for="img-3" class="prev-slide">
+                        <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-5" class="next-slide">
+                        <span>&rsaquo;</span>
+                    </label>
+                </div>
+            </li>
+            ';
         }
         if ($result["image5"] != null) {
-            echo '<label for="img-5" class="carousel-dot" id="img-dot-5"></label>';
+            echo '
+            <input type="radio" name="radio-buttons" id="img-5"/>
+            <li class="slide-container">
+                <div class="slide-image">
+                    <img src="data:image/;base64,'.base64_encode($result["image5"]).'"/>
+                </div>
+                <div class="carousel-controls">
+                    <label for="img-4" class="prev-slide">
+                        <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-6" class="next-slide">
+                        <span>&rsaquo;</span>
+                    </label>
+                </div>
+            </li>
+            ';
         }
-        if ($result["image6"] != null) {
-            echo '<label for="img-6" class="carousel-dot" id="img-dot-6"></label>';
+        if ($result["image5"] != null) {
+            echo '
+            <input type="radio" name="radio-buttons" id="img-6"/>
+            <li class="slide-container">
+                <div class="slide-image">
+                    <img src="data:image/;base64,'.base64_encode($result["image6"]).'"/>
+                </div>
+                <div class="carousel-controls">
+                    <label for="img-5" class="prev-slide">
+                        <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-1" class="next-slide">
+                        <span>&rsaquo;</span>
+                    </label>
+                </div>
+            </li>
+            ';
         }
-        
-        echo '
-    </div>
-        </ul>
-    </div>
-
-<div class="gap"></div>
-
-    <div class="newSubject">
-        <p class="headerText transparant">SUMMARY</p>
-    </div>
-
-    <div class="summaryDiv">
-        <div class="summaryBody">
-            <p class="summaryText transparant">
-                '. $result['blogSummary'] .'
-            </p>
+        echo '<div class="carousel-dots">';
+            if ($result["image"] != null) {
+                echo '<label for="img-1" class="carousel-dot" id="img-dot-1"></label>';
+            }
+            if ($result["image2"] != null) {
+                echo '<label for="img-2" class="carousel-dot" id="img-dot-2"></label>';
+            }
+            if ($result["image3"] != null) {
+                echo '<label for="img-3" class="carousel-dot" id="img-dot-3"></label>';
+            }
+            if ($result["image4"] != null) {
+                echo '<label for="img-4" class="carousel-dot" id="img-dot-4"></label>';
+            }
+            if ($result["image5"] != null) {
+                echo '<label for="img-5" class="carousel-dot" id="img-dot-5"></label>';
+            }
+            if ($result["image6"] != null) {
+                echo '<label for="img-6" class="carousel-dot" id="img-dot-6"></label>';
+            }
+            
+            echo '
         </div>
-    </div>
-    ';
+            </ul>
+        </div>
+
+    <div class="gap"></div>
+
+        <div class="newSubject">
+            <p class="headerText transparant">SUMMARY</p>
+        </div>
+
+        <div class="summaryDiv">
+            <div class="summaryBody">
+                <p class="summaryText transparant">
+                    '. $result['blogSummary'] .'
+                </p>
+            </div>
+        </div>
+        ';
+}
 ?>
